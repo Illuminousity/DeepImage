f7b6ca965e25085c101e50f6789d374d5464bf6b
Illuminousity <matthew.robertson@gaxau.com>
2025-02-10 23:53:53 +0000

    Updated Model

--- Utils/image_diffuser.py
+++ Utils/image_diffuser.py
@@ -96,7 +96,7 @@ def process_image(i):
 # Use ThreadPoolExecutor for parallel execution
 num_workers = 32  # Adjust based on your CPU (recommended: # of CPU cores)
 with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as executor:
-    executor.map(process_image, range(10000))
+    executor.map(process_image, range(1000))
 
 
 
